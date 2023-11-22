621d186e4afa144e210d95abd2a2680cfad88d70
Anishpokhrel04 <anishpokhrel234@gmail.com>
2023-11-22 20:16:40 +0545

    login-signup page has been created

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./SCSS/LoginSignup.scss";
 
 const LoginSignup = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="login-Signup">
+      <div className="login-signup-container">
+        <h1>Sign UP</h1>
 
-export default LoginSignup
+        <div className="login-signup-field">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Your Email" />
+          <input type="password" placeholder="Your Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing , i agree the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignup;
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -36,6 +36,9 @@ const ShopCategory = (props) => {
           }
         })}
       </div>
+      <div className="shopcategory-loadmore">
+        Explore More
+      </div>
     </div>
   );
 };
